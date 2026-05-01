6f1051ba882b3982b60a71a8ac90fe2689c8b3da
MuktadirHQ <57536328+MuktadirHQ@users.noreply.github.com>
2026-05-01 23:14:35 +0600

    syntax_structure

--- syntax_structure.asm
+++ syntax_structure.asm
@@ -3,6 +3,9 @@
 
 .DATA 
     ; Where all the veriable will be declared.
+    
+    NUM1 DB ?
+    
 
 .CODE 
     
@@ -10,9 +13,17 @@
     
     MAIN PROC ; INT MAIN
         
-        ; STATEMENT
-        ; STATEMENT
-        ; STATEMENT
+        ; Instruction Destination, Source
+        ; MOV/ADD/SUB/MUL/DIV, 
+        MOV BH, 4
+        MOV BL, 5
+        
+        ADD BH, BL ; BH = BH + BL
+        ADD BH, 10 ; BH = BH + 10
+        
+        
+        ; FROM DATA VARIABLE : 
+        ADD NUM1, BH ; NUM1 = NUM1 + BH
         
         
         MOV AH, 4CH ; Return 
