da6254e7cc6a1fe3dfa686d0d87b27646f988e3c
MuktadirHQ <57536328+MuktadirHQ@users.noreply.github.com>
2026-08-22 05:09:02 +0600

    OutsideOfMainENDP

---
+++ Procedure.asm
@@ -0,0 +1,46 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+; Declaring our byte array with multi-digit numbers
+my_char db 004DH        ; M
+
+
+.CODE     
+MAIN PROC   
+MOV AX, @DATA
+MOV DS, AX
+           
+           
+           
+MOV DL, my_char
+CALL printCharProc   
+
+
+
+MOV AX, 4C00H
+INT 21H
+MAIN ENDP
+
+
+
+
+
+
+
+
+;============= 
+; Procedure  ; 
+;=============
+
+printCharProc PROC NEAR  ; Default NEAR
+    MOV AH, 2
+    INT 21H
+    RET
+printCharProc ENDP
+
+
+
+
+
+
