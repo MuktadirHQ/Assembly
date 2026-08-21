965f0c28f8c765c401bcc8c4fee3060e462af186
MuktadirHQ <57536328+MuktadirHQ@users.noreply.github.com>
2026-08-22 02:08:49 +0600

    array

---
+++ array.asm
@@ -0,0 +1,35 @@
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+NUM_ARRAY DB 1, 2, 3, 4    ; array
+
+.CODE
+MAIN PROC
+
+; initialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+LEA SI, NUM_ARRAY           ; place B array's first index which is 0, set to SI. 
+
+MOV AL, [SI+1+1+1]          ; AL = NUM_ARRAY[0] = 1
+ADD AL, 48                  ; ASCII
+
+MOV DL, AL                  ; DL stores what to print
+MOV AH, 2
+INT 21H
+
+
+; OUTPUT ==---== 4 
+ 
+
+;exit to DOS     
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
