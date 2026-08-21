c10a0aa9ab257b9bfbd1fe39c04bb3a6c1fba5b5
MuktadirHQ <57536328+MuktadirHQ@users.noreply.github.com>
2026-08-22 02:13:25 +0600

    Access

---
+++ array_access.asm
@@ -0,0 +1,37 @@
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+NUM_ARRAY DB 1, 2, 3, 4    ; array
+
+.CODE
+MAIN PROC
+
+; initialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+LEA SI, NUM_ARRAY           ; place B array's first index which is 0, set to SI.       
+
+MOV BX, 1
+MOV SI, 1
+MOV AL, NUM_ARRAY[BX+SI]                ; AL = NUM_ARRAY[0] = 1
+ADD AL, 48                  ; ASCII
+
+MOV DL, AL                  ; DL stores what to print
+MOV AH, 2
+INT 21H
+
+
+; OUTPUT ==---== 4 
+ 
+
+;exit to DOS     
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
